bf009245447f326fb60d76fedaa2e5d4a3a251f7
galavon911-gif <galavon911@gmail.com>
2026-03-28 23:08:39 +0300

    Revert "update proekt Sprint_2"

--- src/model/constants/Colour.java
+++ src/model/constants/Colour.java
@@ -1,8 +1,8 @@
 package model.constants;
 
 public class Colour {
-    public static final String COLOUR_RED = "red";
-    public static final String COLOUR_GREEN = "green";
+    public static final String colour_red = "red";
+    public static final String colour_green = "green";
 }
 
 
--- src/model/constants/Discount.java
+++ src/model/constants/Discount.java
@@ -2,6 +2,20 @@ package model.constants;
 
 import model.Discountable;
 
-public abstract class Discount implements Discountable {
-    public static final double RED_APPLE_DISCOUNT = 0.6;
+public class Discount implements Discountable {
+    private String colour;
+    private double price;
+
+    public Discount(String colour, double price) {
+        this.colour = colour;
+        this.price = price;
+    }
+
+    @Override
+    public double getDiscount() {
+        if (colour=="red") {
+            return price * 0.4;
+        }
+        return price; // Нет скидки
+    }
 }
