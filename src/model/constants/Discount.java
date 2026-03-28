743bb907f7c61b68c734719e19ff30d0b86119ec
Gal_Grig <galgrig91@yandex.ru>
2026-03-28 22:56:26 +0300

    update proekt Sprint_2

--- src/model/constants/Discount.java
+++ src/model/constants/Discount.java
@@ -2,20 +2,6 @@ package model.constants;
 
 import model.Discountable;
 
-public class Discount implements Discountable {
-    private String colour;
-    private double price;
-
-    public Discount(String colour, double price) {
-        this.colour = colour;
-        this.price = price;
-    }
-
-    @Override
-    public double getDiscount() {
-        if (colour=="red") {
-            return price * 0.4;
-        }
-        return price; // Нет скидки
-    }
+public abstract class Discount implements Discountable {
+    public static final double RED_APPLE_DISCOUNT = 0.6;
 }
